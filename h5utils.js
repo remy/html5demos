25801ef25644eb4e5b456fc8b41b13628a2d62a8
Remy Sharp <remy@remysharp.com>
2010-02-10 08:10:21 +0000

    Fixed binding to window object

--- h5utils.js
+++ h5utils.js
@@ -4,7 +4,7 @@
 var addEvent = (function () {
   if (document.addEventListener) {
     return function (el, type, fn) {
-      if (el && el.nodeName) {
+      if (el && el.nodeName || el === window) {
         el.addEventListener(type, fn, false);
       } else if (el && el.length) {
         for (var i = 0; i < el.length; i++) {
@@ -14,7 +14,7 @@ var addEvent = (function () {
     };
   } else {
     return function (el, type, fn) {
-      if (el && el.nodeName) {
+      if (el && el.nodeName || el === window) {
         el.attachEvent('on' + type, function () { return fn.call(el, window.event); });
       } else if (el && el.length) {
         for (var i = 0; i < el.length; i++) {
