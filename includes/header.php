ad614eef1bfb913431fe2ca43365daf6496aaf34
Remy Sharp <remy@leftlogic.com>
2010-03-04 12:07:09 +0000

    Complete reorganisation of the directory so that making new demos appears, visually, more consistent. I'd like a better way to credit other people if they start contributing demos - but for now, no one has ;-)

---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en"<?=$manifest?>>
+<head>
+<meta charset=utf-8 />
+<title>HTML5 Demo: <?=$title?></title>
+<link rel="stylesheet" href="/css/html5demos.css" type="text/css" />
+<?php if (!$manifest) : ?><script src="/js/h5utils.js"></script><?php endif ?>
+</head>
+<body>
+<section id="wrapper">
+    <header>
+      <h1><?=$title?></h1>
+    </header>
---
+++ page.php
@@ -0,0 +1,22 @@
+<?php
+$request = preg_replace('/[^0-9a-z-_]/', '', preg_replace('/^\//', '', preg_replace('/\/$/', '', preg_replace('/\?.*$/', '', $_SERVER['REQUEST_URI']))));
+if (file_exists('demos/' . $request . '.html')) {
+  $file = file_get_contents('demos/' . $request . '.html');
+  preg_match('/<title>(.*)<\/title>/', $file, $matches);
+  $file = preg_replace('/<title>(.*?)<\/title>/', '', $file);
+  $title = $matches[1];
+
+  $manifest = '';
+  if ($request == 'offlineapp') { // specific change to support <html manifest=xyz>
+    $manifest = ' manifest="html5demo.manifest"';
+  }
+  
+  include('includes/header.php');
+  echo $file;
+  include('includes/footer.php');
+} else {
+  header("HTTP/1.0 404 Not Found");
+  echo 'File not found';
+}
+
+?>
