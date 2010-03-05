d7aa7ced8ac45f9cdf08deae492d8a8e7aa1c380
Remy Sharp <remy@leftlogic.com>
2010-03-05 01:53:40 +0000

    Tweak to viewport header

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,7 @@
 <html lang="en"<?=$manifest?>>
 <head>
 <meta charset=utf-8 />
+<meta name="viewport" content="width=620" />
 <title>HTML5 Demo: <?=$title?></title>
 <link rel="stylesheet" href="/css/html5demos.css" type="text/css" />
 <?php if (!$manifest) : ?><script src="/js/h5utils.js"></script><?php endif ?>
