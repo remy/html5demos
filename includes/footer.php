371d2c493e6b28167bcde7ba557dfdd25c161c4d
Remy Sharp <remy@leftlogic.com>
2010-03-05 02:12:37 +0000

    Put GA back in

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
     <footer><a href="/">HTML5 demos</a>/<a id="built" href="http://twitter.com/rem">@rem built this</a>/<a href="#view-source">view source</a></footer> 
 </section>
 <a href="http://github.com/remy/html5demos"><img style="position: absolute; top: 0; left: 0; border: 0;" src="http://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png" alt="Fork me on GitHub" /></a>
-<!-- <script>
+<script>
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 </script>
@@ -10,6 +10,6 @@ document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.
 try {
 var pageTracker = _gat._getTracker("UA-1656750-18");
 pageTracker._trackPageview();
-} catch(err) {}</script> -->
+} catch(err) {}</script>
 </body>
 </html>
