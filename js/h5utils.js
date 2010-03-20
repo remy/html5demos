8c773e8db2103d52468661b8b22874e3f890a0e0
Remy Sharp <remy@leftlogic.com>
2010-03-20 19:15:31 +0000

    Updated to latest html5 enabler

--- js/h5utils.js
+++ js/h5utils.js
@@ -1,5 +1,6 @@
 // For discussion and comments, see: http://remysharp.com/2009/01/07/html5-enabling-script/
-(function(){if(!/*@cc_on!@*/0)return;var e = "abbr,article,aside,audio,canvas,datalist,details,eventsource,figure,footer,header,hgroup,mark,menu,meter,nav,output,progress,section,time,video".split(','),i=e.length;while (i--){document.createElement(e[i])}})();
+/*@cc_on'abbr article aside audio canvas details figcaption figure footer header hgroup mark menu meter nav output progress section summary time video'.replace(/\w+/g,function(n){document.createElement(n)})@*/
+
 
 var addEvent = (function () {
   if (document.addEventListener) {
