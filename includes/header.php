bb3f9948f60f84a49476a88ac523c688250f92b6
Remy Sharp <remy@remysharp.com>
2012-02-16 22:46:21 +0000

    ads: someone's gotta pay the bills

--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,7 @@
 </head>
 <body>
 <section id="wrapper">
+<div id="carbonads-container"><div class="carbonad"><div id="azcarbon"></div><script type="text/javascript">var z = document.createElement("script"); z.type = "text/javascript"; z.async = true; z.src = "http://engine.carbonads.com/z/14060/azcarbon_2_1_0_VERT"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(z, s);</script></div></div>
     <header>
       <h1><?=$title?></h1>
     </header>
