c0fd46128b83ad2115be1faa2f97de6dfdd072a5
Tady Walsh <tadywalsh@gmail.com>
2015-02-19 17:11:53 +0100

    Changed JS on Storage page as it wasn't local and session values on the page were not updating on Keyup

--- page.php
+++ page.php
@@ -1,22 +1,28 @@
 <?php
-$request = preg_replace('/[^0-9a-z-_]/', '', preg_replace('/^\//', '', preg_replace('/\/$/', '', preg_replace('/\?.*$/', '', $_SERVER['REQUEST_URI']))));
-if (file_exists('demos/' . $request . '.html')) {
-  $file = file_get_contents('demos/' . $request . '.html');
-  preg_match('/<title>(.*)<\/title>/', $file, $matches);
-  $file = preg_replace('/<title>(.*?)<\/title>/', '', $file);
-  $title = $matches[1];
+// $request = preg_replace('/[^0-9a-z-_]/', '', preg_replace('/^\//', '', preg_replace('/\/$/', '', preg_replace('/\?.*$/', '', $_SERVER['REQUEST_URI']))));
 
-  $manifest = '';
-  if ($request == 'offlineapp') { // specific change to support <html manifest=xyz>
-    $manifest = ' manifest="html5demo.appcache"';
+if($request= end((explode('/', $_SERVER['REQUEST_URI']))))
+{
+  if (file_exists('demos/' . $request . '.html')) {
+    $file = file_get_contents('demos/' . $request . '.html');
+    preg_match('/<title>(.*)<\/title>/', $file, $matches);
+    $file = preg_replace('/<title>(.*?)<\/title>/', '', $file);
+    $title = $matches[1];
+
+    $manifest = '';
+    if ($request == 'offlineapp') { // specific change to support <html manifest=xyz>
+      $manifest = ' manifest="html5demo.appcache"';
+    }
+    
+    include('includes/header.php');
+    echo $file;
+    include('includes/footer.php');
+  } else {
+    header("HTTP/1.0 404 Not Found");
+    echo 'File not found';
   }
-  
-  include('includes/header.php');
-  echo $file;
-  include('includes/footer.php');
 } else {
-  header("HTTP/1.0 404 Not Found");
-  echo 'File not found';
+  echo "Invalid Request";
 }
 
 ?>
